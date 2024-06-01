282846732ac73d9e53508a3e1ef39f15a053588a
RODRICL <carreonlambertinrodrigo@gmail.com>
2024-06-01 00:14:13 -0400

    parcial 2

---
+++ be_p2_rcl/src/series/entities/series.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+ @Entity('Series')
+export class Series {
+    //poner llave primaria
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    
+    @Column({type: 'varchar',length:250,nullable:false})
+    titulo: string ;
+
+    
+    @Column({type: 'varchar',length:5000,nullable:false})
+    sinopsis:string ;
+
+    
+    @Column({type: 'varchar',length:100,nullable:false})
+    director :string;
+
+    
+    @Column()
+    temporada: number;
+
+    @Column({name:'fecha_estreno'})
+    fechaEstreno:Date;
+
+}
